e0d5d61326df140f3a6ff7389bf3d7c164385192
Yegor Bitensky <yegorbitensky@gmail.com>
2020-09-04 01:49:21 +0300

    add task

--- node/app/gulpfile.js
+++ node/app/gulpfile.js
@@ -1,6 +1,14 @@
-function defaultTask(cb) {
-  cb();
+const { src, dest } = require('gulp');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify-es').default;
+const rename = require('gulp-rename');
+
+exports.default = function() {
+  return src('*.js')
+    .pipe(babel())
+    // .pipe(src('src/*.js'))
+    .pipe(dest('output/'))
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(dest('output/'));
 }
-
-
-exports.default = defaultTask
